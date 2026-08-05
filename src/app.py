e85805b6bdad565257c0b5faf5c6566c8d48c379
hagitshomrony-orca <hagit.shomrony@orca.security>
2026-08-05 14:10:19 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 def get_user(user_id):
-    conn = sqlite3.connect("app.db")
+    conn = sqlite3.connect("app1.db")
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM users WHERE id = {user_id}")
     return cursor.fetchone()
